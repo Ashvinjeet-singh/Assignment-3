e7807b39973491e212a30e4e9578d8c90e0f0ac8
Ashvinjeet-singh <ashvinjeet_singh@softprodigy.com>
2020-02-06 13:19:34 +0530

    Jquery Somewhat Working

--- JS/main.js
+++ JS/main.js
@@ -1,62 +1,79 @@
 $(document).ready(function()
+{   
+    $(".blogheader").fadeOut()
+    $(".blog-card1,.blog-card2,.blog-card3,.brandheader,.cfmbtn,.collfadeIN,.card,.experts,#abouth,#para1,#para2,.cap2,.cap1,.cap3").hide()
+    
+    $(".experts").animate(
+        {
+            right:"100px"
+        }
+    )
+    
+   
+
+
+    // blog -animation
+    $(".parablog").mouseenter( function(){
+        $(".blogheader").fadeIn(1000);
+        $(".blog-card1").slideDown(1500)
+        $(".blog-card2").slideDown(1500);
+        $(".blog-card3").slideDown(1500);
+    });
+
+
+
+// brands available
+$(".brandslide").mouseenter(function()
 {
-    $("#abouth,#para1,#para2,#our_team,.card,#gh,.qwe").hide();
-
-    $(".qwe").show(500)
-
-    $(".qwe").mouseenter(function()
-    {   
-        $("#abouth").fadeIn(1000);
-        console.log("Ghsakdsald");
-    }
-    );
-
-    $(".qwe").mouseenter(function()
-    {   
-        $("#para1").slideDown(2000);
-        console.log("Ghsakdadsasdsasald");
-    }
-    );
-
-    $(".qwe").mouseenter(function()
-    {   
-        $("#para2").slideDown(2000);
-        console.log("Ghsakdsaldadas");
-    }
-    );
+    $(".brandheader").slideDown(1000)
+});
 
 
-    $(".people").mouseenter(function()
-    {  
-        $("#our_team").animate
-        (
-            {left:"100px"}
-        )
-        $("#our_team").show(1000)
-        $("#our_team").animate
-        (
-            {right:"100px"}
-        )
+//collection section
+
+$(".Collsection").mouseenter(function()
+{
+    //click for more btn
+    $(".cfmbtn").fadeIn(2000);
+
+    //nike air jordan x off white
+    $(".collfadeIN").fadeIn(2000);
+});
+
+//people section
+
+$(".people").mouseenter(function()
+{
+    $(".card").slideDown(1500)
+    
+    $(".experts").animate(
+        {   
+            left:"50px",
+            opacity:"show"
+            
+        },3000
+    )
+});
 
 
+//about section
 
-        $("#gh").animate
-        (
-            {left:"100px"}
-        )
-        $("#gh").show(1000)
-        $("#gh").animate
-        (
-            {right:"100px"}
-        )
+$(".paraabout").mouseenter(function()
+{
+    $("#abouth").slideDown(1500);
+    $("#para1").fadeIn(1500);
+    $("#para2").fadeIn(1500);
+    
+});
 
-        $(".card").slideDown(2000)
+//slider
+$(".head1").mouseenter(function()
+{
+    $(".cap2").fadeIn(1500);
+    console.log("haed1")
+    $(".cap1").fadeIn(4000);
+    $(".cap3").fadeIn(4000);
+    
+});
 
-    }
-    );
-
-
-}
-);
-
-
+});
